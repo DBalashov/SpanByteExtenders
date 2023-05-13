995253b81bf2266bce50dbe63097c24e449c2a8b
balashov <dbalashov@gmail.com>
2023-05-13 22:20:25 +0400

    initial

---
+++ SpanArrayExtenders.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SpanByteExtenders;
+
+public static class SpanArrayExtenders
+{
+    public static Span<T> readItems<T>(this ref Span<byte> span, int count, int itemSize) where T : struct
+    {
+        if (count < 0) throw new ArgumentException("Must be >0", nameof(count));
+
+        var r = MemoryMarshal.Cast<byte, T>(span.Slice(count * itemSize));
+        span = span.Slice(count * itemSize);
+        return r;
+    }
+
+    public static Span<Guid> ReadGuids(this ref Span<byte> span, int count) => span.readItems<Guid>(count, 16);
+
+    public static Span<double> ReadDoubles(this ref Span<byte> span, int count) => span.readItems<double>(count, 8);
+
+    public static Span<float> ReadFloats(this ref Span<byte> span, int count) => span.readItems<float>(count, 4);
+
+    public static Span<decimal> ReadDecimals(this ref Span<byte> span, int count)
+    {
+        var r = new decimal[count];
+        for (var i = 0; i < count; i++)
+            r[i] = span.ReadDecimal();
+        return r;
+    }
+
+    public static Span<Int64> ReadInt64s(this ref Span<byte> span, int count) => span.readItems<Int64>(count, 8);
+
+    public static Span<UInt64> ReadUInt64s(this ref Span<byte> span, int count) => span.readItems<UInt64>(count, 8);
+
+    public static Span<int> ReadInt32s(this ref Span<byte> span, int count) => span.readItems<int>(count, 4);
+
+    public static Span<uint> ReadUInt32s(this ref Span<byte> span, int count) => span.readItems<uint>(count, 4);
+
+    public static Span<short> ReadInt16s(this ref Span<byte> span, int count) => span.readItems<short>(count, 2);
+
+    public static Span<ushort> ReadUInt16s(this ref Span<byte> span, int count) => span.readItems<ushort>(count, 2);
+
+    public static Span<sbyte> ReadSBytes(this ref Span<byte> span, int count) => span.readItems<sbyte>(count, 1);
+
+    public static Span<byte> ReadBytes(this ref Span<byte> span, int count)
+    {
+        var r = span.Slice(0, count);
+        span = span.Slice(count);
+        return r;
+    }
+}
---
+++ SpanSimpleExtenders.cs
@@ -0,0 +1,84 @@
+﻿namespace SpanByteExtenders;
+
+public static class SpanSimpleExtenders
+{
+    public static Guid ReadGuid(this ref Span<byte> span)
+    {
+        var r = new Guid(span.Slice(0, 16));
+        span = span.Slice(16);
+        return r;
+    }
+
+    public static double ReadDouble(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToDouble(span);
+        span = span.Slice(8);
+        return r;
+    }
+
+    public static float ReadFloat(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToUInt64(span);
+        span = span.Slice(4);
+        return r;
+    }
+    
+    public static decimal ReadDecimal(this ref Span<byte> span) => 
+        new (span.ReadInt32s(4));
+
+    public static Int64 ReadInt64(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToInt64(span);
+        span = span.Slice(8);
+        return r;
+    }
+
+    public static UInt64 ReadUInt64(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToUInt64(span);
+        span = span.Slice(8);
+        return r;
+    }
+
+    public static int ReadInt32(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToInt32(span);
+        span = span.Slice(4);
+        return r;
+    }
+
+    public static uint ReadUInt32(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToUInt32(span);
+        span = span.Slice(4);
+        return r;
+    }
+
+    public static short ReadInt16(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToInt16(span);
+        span = span.Slice(2);
+        return r;
+    }
+
+    public static ushort ReadUInt16(this ref Span<byte> span)
+    {
+        var r = BitConverter.ToUInt16(span);
+        span = span.Slice(2);
+        return r;
+    }
+
+    public static sbyte ReadSByte(this ref Span<byte> span)
+    {
+        var b = span[0];
+        span = span.Slice(1);
+        return (sbyte) b;
+    }
+
+    public static byte ReadByte(this ref Span<byte> span)
+    {
+        var b = span[0];
+        span = span.Slice(1);
+        return b;
+    }
+}
